b3de1ae6d538669c59bb33a376a3e17a2e4b05e2
luisjrjr11 <153125617+luisjrjr11@users.noreply.github.com>
2026-09-01 19:24:28 -0500

    libro.py

--- src/libro.py
+++ src/libro.py
@@ -1,1 +1,10 @@
-# Código del módulo
+class Libro:
+    def __init__(self, titulo, autor, isbn):
+        self.titulo = titulo
+        self.autor = autor
+        self.isbn = isbn
+
+def registrar_libro(titulo, autor, isbn):
+    if not titulo or not autor or not isbn:
+        return False
+    return Libro(titulo, autor, isbn)
